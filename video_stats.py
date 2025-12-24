10cb3e546def9ffa0874257327322691cedcdd7e
samur-rahman <ahotasam@buffalo.edu>
2025-12-23 23:49:26 -0500

    Added getVideoIDs function to make the list of videos in the channel.

--- video_stats.py
+++ video_stats.py
@@ -8,6 +8,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 
 def getPlaylistID():
@@ -31,5 +32,41 @@ def getPlaylistID():
         raise e
     
 
+def getVideoIDs(playlist_id):
+    video_ids = []
+    
+    page_token = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlist_id}&key={API_KEY}"
+
+    try:
+        while True:
+            url = base_url
+
+            if page_token:
+                url += f"&pageToken={page_token}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            page_token = data.get('nextPageToken')
+
+            if not page_token:
+                break
+
+        return video_ids
+
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
 if __name__ == "__main__":
-    getPlaylistID()
+    playListID = getPlaylistID()
+    getVideoIDs(playListID)
