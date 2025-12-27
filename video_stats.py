fc1bb57b557e9cfe0444730b4998f4af8345044e
samur-rahman <ahotasam@buffalo.edu>
2025-12-26 20:47:31 -0500

    Added the function for extracting video data such as video_id, title, publishedAt, duration, viewCount, like and comment count.

--- video_stats.py
+++ video_stats.py
@@ -67,6 +67,52 @@ def getVideoIDs(playlist_id):
         raise e
 
 
+def extractVideoData(video_ids):
+    extracted_data = []
+
+
+    def batchList(video_id_list, batch_size):
+        # Yield successive batches (sublists) from `video_id_list` (max size `batch_size`).
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id: video_id + batch_size]
+
+    try:
+        for batch in batchList(video_ids, maxResults):
+            video_ids_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+            
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet['publishedAt'],
+                    "duration": contentDetails['duration'],
+                    "viewCount": statistics.get('viewCount', None),
+                    "likeCount": statistics.get('likeCount', None),
+                    "commentCount": statistics.get('commentCount', None),
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+
 if __name__ == "__main__":
     playListID = getPlaylistID()
-    getVideoIDs(playListID)
+    video_ids = getVideoIDs(playListID)
+    extractVideoData(video_ids)
