01c740565a7dc881a7774c988e1d1ea782a338df
samur-rahman <ahotasam@buffalo.edu>
2026-02-18 22:42:37 -0500

    feat(warehouse): implement database initialization and ingestion helpersAdded functions to create schema, create tables, and retrieve video IDsto support structured storage for Airflow ingestion pipeline.

--- dags/datawarehouse/data_utils.py
+++ dags/datawarehouse/data_utils.py
@@ -1,6 +1,8 @@
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from psycopg2.extras import RealDictCursor
 
+table = "yt_api"
+
 
 def getConnCursor():
     hook = PostgresHook(postgres_conn_id="postgres_db_yt_eld", database="elt_db")
@@ -12,3 +14,57 @@ def getConnCursor():
 def closeConnCursor(conn, cur):
     cur.close()
     conn.close()
+
+
+def createSchema(schema):
+    conn, cur = getConnCursor()
+
+    schema_sql = f"CREATE SCHEMA IF NOT EXISTS {schema};"
+    cur.execute(schema_sql)
+
+    conn.commit()
+    closeConnCursor(conn, cur)
+
+
+def createTable(schema):
+    conn, cur = getConnCursor()
+
+    if schema == 'staging':
+        table_sql = f"""
+                    CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                        "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                        "Video_Title" TEXT NOT NULL,
+                        "Upload_Date" TIMESTAMP NOT NULL,
+                        "Duration" VARCHAR(20) NOT NULL,
+                        "Video_Views" INT,
+                        "Likes_Count" INT,
+                        "Comments_Count" INT   
+                    );
+                """
+    else:
+        table_sql = f"""
+                  CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                      "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                      "Video_Title" TEXT NOT NULL,
+                      "Upload_Date" TIMESTAMP NOT NULL,
+                      "Duration" TIME NOT NULL,
+                      "Video_Type" VARCHAR(10) NOT NULL,
+                      "Video_Views" INT,
+                      "Likes_Count" INT,
+                      "Comments_Count" INT    
+                  ); 
+              """        
+
+    cur.execute(table_sql)
+
+    conn.commit()
+    closeConnCursor(conn, cur)
+
+
+def getVideoIds(cur, schema):
+    cur.execute(f"""SELECT "Video_ID" FROM {schema}.{table};""")
+    ids = cur.fetchall()
+
+    video_ids = [row["Video_ID"] for row in ids]
+
+    return video_ids
